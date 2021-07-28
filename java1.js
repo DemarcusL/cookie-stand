6de82030b0ce21d2a4fba2c082bc0999cac00a89
Demarcus Lester <demarcuslester12@gmail.com>
2021-07-28 10:06:57 -0500

    Text is being displayed now, numbers arent

--- java1.js
+++ java1.js
@@ -21,13 +21,13 @@ let seattleStore = // define object literal
     //deteremine hours open 6am - 8pm, how many customers to expect
 
     //this is a method cause it is in a object box
-    getRandomNumOfCustomerGivenRange: function () {// we need a function for this
+    getCustomerPerHour: function () {// we need a function for this
 
         for (let index = 0; index < openHours.length; index++) {
             this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
 
         }
-        console.log("The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}");
+        console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
 
     },
 
@@ -62,206 +62,211 @@ let seattleStore = // define object literal
 
 }
 
-// let tokyoStore = // define object literal
-// {   //define props
-//     minimumCustomers: 3,
-//     maximumCustomers: 24,
-//     adverageCookieSale: 1.2,
-//     customersPerHour: [],
-//     cookiesSoldPerHour: [],
-//     customersPerHour() {
-//         return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
-//     },
-//     //this is a method cause it is in a object box
-//     getRandomNumOfCustomerGivenRange: function () {
+let tokyoStore = // define object literal
+{   //define props
+    minimumCustomers: 3,
+    maximumCustomers: 24,
+    adverageCookieSale: 1.2,
+    customersPerHour: [],
+    cookiesSoldPerHour: [],
+    customersPerHour() {
+        return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
+    },
+     //this is a method cause it is in a object box
+    getCustomerPerHour: function () {// we need a function for this
 
-//         for (let index = 0; index < openHours.length; index++) {
-//             this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
+        for (let index = 0; index < openHours.length; index++) {
+            this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
 
-//         }
-//         console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
+        }
+        console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
 
-//     },
+    },
 
-//     //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
-//     getCookiesSoldPerHour: function() {
-//         totalDailyCookies = 0;
-//         this.getCustomerPerHour();
-//         for (let index = 0; index < this.customersPerHour.length; index++) {
-//             let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
+    //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
+    //still a method
+    getCookiesSoldPerHour: function() {
+        totalDailyCookies = 0;// set to 0 to start off
+        this.getCustomerPerHour();
+        for (let index = 0; index < this.customersPerHour.length; index++) {
+            let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
 
-//             this.cookiesSoldPerHour.push(dailyCookies);
+            this.cookiesSoldPerHour.push(dailyCookies);
 
-//             this.totalDailyCookies+=dailyCookies;
-//          }
-//     },
+            this.totalDailyCookies+=dailyCookies;
+        }
+    },
 
-//     render() {
-//         this.getCookiesSoldPerHour();
-//         const unorderedList = document.getElementById('tokyo');
-//         for (let i = 0; i < openHours.length; i++) 
-//         {
-//             const listItem = document.createElement('li');
-//             // 6am: 16 cookies
-//             listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
-//             unorderedList.appendChild(listItem);
-//         }
-//         const listItem = document.createElement('li');
-//         listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
-//         unorderedList.appendChild(listItem);
-//     }
-// }
+    //Used to get the data generated to print to the sales html from Kev's walkthrough
+    render() {
+        this.getCookiesSoldPerHour();
+        const unorderedList = document.getElementById('seattle');// this is id'd in the html
+        for (let i = 0; i < openHours.length; i++) //this will keep iterating until the hours are met
+        {
+            const listItem = document.createElement('li'); //make listed items
+            listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies'; 
+            unorderedList.appendChild(listItem);
+        }
+        const listItem = document.createElement('li');
+        listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+}
 
-// let dubaiStore = // define object literal
-// {   //define props
-//     minimumCustomers: 11,
-//     maximumCustomers: 38,
-//     adverageCookieSale: 3.7,
-//     customersPerHour: [],
-//     cookiesSoldPerHour: [],
-//     customersPerHour() {
-//         return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
-//     },
-//     //this is a method cause it is in a object box
-//     getRandomNumOfCustomerGivenRange: function () {
+let dubaiStore = // define object literal
+{   //define props
+    minimumCustomers: 11,
+    maximumCustomers: 38,
+    adverageCookieSale: 3.7,
+    customersPerHour: [],
+    cookiesSoldPerHour: [],
+    customersPerHour() {
+        return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
+    },
+     //this is a method cause it is in a object box
+    getCustomerPerHour: function () {// we need a function for this
 
-//         for (let index = 0; index < openHours.length; index++) {
-//             this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
+        for (let index = 0; index < openHours.length; index++) {
+            this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
 
-//         }
-//         console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
+        }
+        console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
 
-//     },
+    },
 
-//     //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
-//     getCookiesSoldPerHour: function() {
-//         totalDailyCookies = 0;
-//         this.getCustomerPerHour();
-//         for (let index = 0; index < this.customersPerHour.length; index++) {
-//             let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
+    //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
+    //still a method
+    getCookiesSoldPerHour: function() {
+        totalDailyCookies = 0;// set to 0 to start off
+        this.getCustomerPerHour();
+        for (let index = 0; index < this.customersPerHour.length; index++) {
+            let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
 
-//             this.cookiesSoldPerHour.push(dailyCookies);
+            this.cookiesSoldPerHour.push(dailyCookies);
 
-//             this.totalDailyCookies+=dailyCookies;
-//          }
-//     },
+            this.totalDailyCookies+=dailyCookies;
+        }
+    },
 
-//     render() {
-//         this.getCookiesSoldPerHour();
-//         const unorderedList = document.getElementById('dubai');
-//         for (let i = 0; i < openHours.length; i++) 
-//         {
-//             const listItem = document.createElement('li');
-//             // 6am: 16 cookies
-//             listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
-//             unorderedList.appendChild(listItem);
-//         }
-//         const listItem = document.createElement('li');
-//         listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
-//         unorderedList.appendChild(listItem);
-//     }
-// }
+    //Used to get the data generated to print to the sales html from Kev's walkthrough
+    render() {
+        this.getCookiesSoldPerHour();
+        const unorderedList = document.getElementById('seattle');// this is id'd in the html
+        for (let i = 0; i < openHours.length; i++) //this will keep iterating until the hours are met
+        {
+            const listItem = document.createElement('li'); //make listed items
+            listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies'; 
+            unorderedList.appendChild(listItem);
+        }
+        const listItem = document.createElement('li');
+        listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+}
 
-// let parisStore = // define object literal
-// {   //define props
-//     minimumCustomers: 20,
-//     maximumCustomers: 38,
-//     adverageCookieSale: 2.3,
-//     customersPerHour: [],
-//     cookiesSoldPerHour: [],
-//     customersPerHour: [],
-//     cookiesSoldPerHour: [],
-//     customersPerHour() {
-//         return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
-//     },
-//     //this is a method cause it is in a object box
-//     getRandomNumOfCustomerGivenRange: function () {
+let parisStore = // define object literal
+{   //define props
+    minimumCustomers: 20,
+    maximumCustomers: 38,
+    adverageCookieSale: 2.3,
+    customersPerHour: [],
+    cookiesSoldPerHour: [],
+    customersPerHour: [],
+    cookiesSoldPerHour: [],
+    customersPerHour() {
+        return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
+    },
+    //this is a method cause it is in a object box
+    getCustomerPerHour: function () {// we need a function for this
 
-//         for (let index = 0; index < openHours.length; index++) {
-//             this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
+        for (let index = 0; index < openHours.length; index++) {
+            this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
 
-//         }
-//         console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
+        }
+        console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
 
-//     },
+    },
 
-//     //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
-//     getCookiesSoldPerHour: function() {
-//         totalDailyCookies = 0;
-//         this.getCustomerPerHour();
-//         for (let index = 0; index < this.customersPerHour.length; index++) {
-//             let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
+    //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
+    //still a method
+    getCookiesSoldPerHour: function() {
+        totalDailyCookies = 0;// set to 0 to start off
+        this.getCustomerPerHour();
+        for (let index = 0; index < this.customersPerHour.length; index++) {
+            let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
 
-//             this.cookiesSoldPerHour.push(dailyCookies);
+            this.cookiesSoldPerHour.push(dailyCookies);
 
-//             this.totalDailyCookies+=dailyCookies;
-//          }
-//     },
+            this.totalDailyCookies+=dailyCookies;
+        }
+    },
 
-//     render() {
-//         this.getCookiesSoldPerHour();
-//         const unorderedList = document.getElementById('paris');
-//         for (let i = 0; i < openHours.length; i++) 
-//         {
-//             const listItem = document.createElement('li');
-//             // 6am: 16 cookies
-//             listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
-//             unorderedList.appendChild(listItem);
-//         }
-//         const listItem = document.createElement('li');
-//         listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
-//         unorderedList.appendChild(listItem);
-//     }
-// }
+    //Used to get the data generated to print to the sales html from Kev's walkthrough
+    render() {
+        this.getCookiesSoldPerHour();
+        const unorderedList = document.getElementById('seattle');// this is id'd in the html
+        for (let i = 0; i < openHours.length; i++) //this will keep iterating until the hours are met
+        {
+            const listItem = document.createElement('li'); //make listed items
+            listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies'; 
+            unorderedList.appendChild(listItem);
+        }
+        const listItem = document.createElement('li');
+        listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+}
 
-// let limaStore = // define object literal
-// {   //define props
-//     minimumCustomers: 2,
-//     maximumCustomers: 16,
-//     adverageCookieSale: 4.6,
-//     customersPerHour: [],
-//     cookiesSoldPerHour: [],
-//     customersPerHour() {
-//         return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
-//     },
-//     //this is a method cause it is in a object box
-//     getRandomNumOfCustomerGivenRange: function () {
+let limaStore = // define object literal
+{   //define props
+    minimumCustomers: 2,
+    maximumCustomers: 16,
+    adverageCookieSale: 4.6,
+    customersPerHour: [],
+    cookiesSoldPerHour: [],
+    customersPerHour() {
+        return Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
+    },
+     //this is a method cause it is in a object box
+    getCustomerPerHour: function () {// we need a function for this
 
-//         for (let index = 0; index < openHours.length; index++) {
-//             this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
+        for (let index = 0; index < openHours.length; index++) {
+            this.customersPerHour.push(getRandomNumOfCustomerGivenRange(this.minimumCustomers, this.maximumCustomers));
 
-//         }
-//         console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
+        }
+        console.log(`The min value is ${this.minimumCustomers} the max value is ${this.maximumCustomers}`);
 
-//     },
+    },
 
-//     //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
-//     getCookiesSoldPerHour: function() {
-//         totalDailyCookies = 0;
-//         this.getCustomerPerHour();
-//         for (let index = 0; index < this.customersPerHour.length; index++) {
-//             let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
+    //We are writting the functions now. We havent CALLED THE FUNCTION them yet by not writting a paran
+    //still a method
+    getCookiesSoldPerHour: function() {
+        totalDailyCookies = 0;// set to 0 to start off
+        this.getCustomerPerHour();
+        for (let index = 0; index < this.customersPerHour.length; index++) {
+            let dailyCookies = Math.floor(this.customersPerHour[index] * this.avgCookieSale);
 
-//             this.cookiesSoldPerHour.push(dailyCookies);
+            this.cookiesSoldPerHour.push(dailyCookies);
 
-//             this.totalDailyCookies+=dailyCookies;
-//          }
-//     },
+            this.totalDailyCookies+=dailyCookies;
+        }
+    },
 
-//     render() {
-//         this.getCookiesSoldPerHour();
-//         const unorderedList = document.getElementById('lima');
-//         for (let i = 0; i < openHours.length; i++) 
-//         {
-//             const listItem = document.createElement('li');
-//             // 6am: 16 cookies
-//             listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies';
-//             unorderedList.appendChild(listItem);
-//         }
-//         const listItem = document.createElement('li');
-//         listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
-//         unorderedList.appendChild(listItem);
-//     }
-// }
+    //Used to get the data generated to print to the sales html from Kev's walkthrough
+    render() {
+        this.getCookiesSoldPerHour();
+        const unorderedList = document.getElementById('seattle');// this is id'd in the html
+        for (let i = 0; i < openHours.length; i++) //this will keep iterating until the hours are met
+        {
+            const listItem = document.createElement('li'); //make listed items
+            listItem.textContent = openHours[i] + ': ' + this.cookiesSoldPerHour[i] + ' cookies'; 
+            unorderedList.appendChild(listItem);
+        }
+        const listItem = document.createElement('li');
+        listItem.textContent = 'Total: ' + this.totalDailyCookies + ' cookies';
+        unorderedList.appendChild(listItem);
+    }
+}
 
 seattleStore.render();
+tokyoStore.render();
